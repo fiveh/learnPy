864c7dbabef88e274298d3be770c7017bd9c8631
Andrew <hunter.hunternabab@gmail.com>
2018-03-23 10:58:12 +0300

    up data

--- main/medPY_test.py
+++ main/medPY_test.py
@@ -9,7 +9,7 @@ import matplotlib.pyplot as plt
 
 
 # showing with cs2
-def showingGif(path):
+def showing_gif(path):
     img = sitk.ReadImage(path)
     for i in range(0, 155):
         # print("frame ", i)
@@ -22,9 +22,9 @@ def showingGif(path):
         cv2.waitKey(10)
 
 
-# FIXME:  fix size of 'for' function
+# FIXME:  fix size of 'for' function, because not all files is printing
 # showing with plt.show with uint8
-def showingGifPlt1(path):
+def showing_gif_plt1(path):
     img = sitk.ReadImage(path)
     # for i in range(0, 155):
     for i in range(90, 100):
@@ -39,35 +39,48 @@ def showingGifPlt1(path):
     # plt.re
 
 
+# FIXME:  choose the main images from 0 to 155 frames
 # save images  to folder
-def saveImage(path):
+def save_image(path, name_for_file):
     img = sitk.ReadImage(path)
-    for i in range(0, 155):
+    # for i in range(0, 155):
+    for i in range(85, 105):
         print("frame ", i)
         tile = sitk.Tile(img[:, :, i])
         nptile = sitk.GetArrayFromImage(tile)
-        # plt.imshow(nptile)
-        # plt.show()
-        filename = str(i)+".jpg"
-        path_to_folder = "../data/med_images/output_image/"+filename
+        # filename = str(i) + name_for_file + ".jpg"  # save all files
+        filename = str(i) + ".jpg"  # reWrite files
+        path_to_folder = "../data/med_images/output_image/" + filename
         plt.imsave(path_to_folder, nptile)
 
 
 # open All files in each folder
-def openAllFiles():
-    for filename in os.listdir("D:/BRATS2015/training/HGG"):
-        for filenamein in os.listdir("D:/BRATS2015/training/HGG/"+filename):
-            print(filenamein)
-            showingGif("D:/BRATS2015/training/HGG/"+filename+"/"+filenamein)
+def open_all_files(path):  # open files to Folder BRATS2015
+    count_files = 0
+    for first_lvl in os.listdir(path):
+        print("1)", first_lvl)
+        for second_lvl in os.listdir(path + first_lvl):
+            print("\t2)", second_lvl)
+            for third_lvl in os.listdir(path + first_lvl + "/" + second_lvl):
+                path_to_file = path + first_lvl + "/" + second_lvl + "/" + third_lvl
+                print("\t\t3)", third_lvl)
+                save_image(path_to_file, third_lvl)
+                # showing_gif(path_to_file)
+                count_files += 1
+
+    print("count files: ", count_files)  # all count files
 
 
-
-path0 = '../data/med_images/VSD.Brain.XX.O.MR_Flair.54193.mha'
-path1 = '../data/med_images/VSD.Brain.XX.O.MR_T1.54194.mha'
-path2 = '../data/med_images/VSD.Brain.XX.O.MR_T1c.54195.mha'
-path3 = '../data/med_images/VSD.Brain.XX.O.MR_T2.54196.mha'
+if __name__ == "__main__":
 
 
-# saveImage(path0)
-# saveImage(path1)
-# saveImage(path2)
+    path_training = "../data/med_images/input_image/BRATS2015/training/"  # ../BRATS2015/training/
+    open_all_files(path_training)
+
+    # path0 = "../data/med_images/input_image/BRATS2015/training/HGG/brats_2013_pat0001_1/VSD.Brain_3more.XX.O.OT.54517.mha"
+    # path1 = "../data/med_images/input_image/BRATS2015/training/HGG/brats_2013_pat0001_1/VSD.Brain.XX.O.MR_Flair.54512.mha"
+    # showing_gif(path0)
+    # save_image(path0)
+    # showing_gif(path1)
+    # save_image(path1)
+

24014032cce39a929c9b03cccf023e57f0c4124f
Andrew <hunter.hunternabab@gmail.com>
2018-08-05 15:11:37 +0300

    except files fith bad object

--- main/medPY_test.py
+++ main/medPY_test.py
@@ -1,21 +1,23 @@
 # TODO: try to run data on Keras CNN
 # TODO: take the more important data from 155 set
 
-import SimpleITK as sitk
-import numpy as np
 import os
+
+import SimpleITK as sitk
 import cv2
 import matplotlib.pyplot as plt
+import numpy as np
 
 CONST_FROM = 90
 CONST_TO = 100
+
 
 # showing with cs2
 def showing_gif(path):
     img = sitk.ReadImage(path)
     # print(img)
-    for i in range(0, 155):
-        print("frame ", i)
+    for i in range(CONST_FROM, CONST_TO):
+        # print("frame ", i)
         tile = sitk.Tile(img[:, :, i])
         nptile = sitk.GetArrayFromImage(tile)
         cv2.imshow("tile", np.uint8(nptile))
@@ -27,7 +29,7 @@ def showing_gif_plt1(path):
     img = sitk.ReadImage(path)
     # for i in range(0, 155):
     for i in range(CONST_FROM, CONST_TO):
-        print("frame ", i)
+        # print("frame ", i)
         tile = sitk.Tile(img[:, :, i])
         nptile = sitk.GetArrayFromImage(tile)
         plt.imshow(np.uint8(nptile))
@@ -42,7 +44,7 @@ def showing_gif_plt1(path):
 def save_image(path, name_for_file):
     img = sitk.ReadImage(path)
     for i in range(CONST_FROM, CONST_TO):
-        print("frame ", i)
+        # print("frame ", i)
         tile = sitk.Tile(img[:, :, i])
         nptile = sitk.GetArrayFromImage(tile)
         filename = name_for_file + str(i) + ".jpg"  # save all files
@@ -55,24 +57,30 @@ def save_image(path, name_for_file):
 # open All files in each folder
 def open_all_files(path):  # open files to Folder BRATS2015
     count_files = 0
+    yes_count = 0
     for first_lvl in os.listdir(path):
         print("1)", first_lvl)
         for second_lvl in os.listdir(path + first_lvl):
             print("\t2)", second_lvl)
             for third_lvl in os.listdir(path + first_lvl + "/" + second_lvl):
                 path_to_file = path + first_lvl + "/" + second_lvl + "/" + third_lvl
+
                 print("\t\t3)", third_lvl)
-                save_image(path_to_file, third_lvl)
+
+                if (path_to_file.find('more.XX') != -1 &
+                        path_to_file.find('.OT.') != -1):
+                    yes_count += 1
+
+                # save_image(path_to_file, third_lvl)
                 # showing_gif(path_to_file)
                 # showing_gif_plt1(path_to_file)
                 count_files += 1
 
     print("count files: ", count_files)  # all count files
+    print("count more.XX files: ", yes_count)  # all count files
 
 
 if __name__ == "__main__":
-
-
     # path_training = "../data/med_images/input_image/BRATS2015/training/"  # ../BRATS2015/training/
     # open_all_files(path_training)
 
@@ -86,10 +94,12 @@ if __name__ == "__main__":
     # showing_gif(path0)
     # save_image(path0, "name1")
     # showing_gif_plt1(path1)
-    save_image(Flair, "name1")
-    save_image(T1, "name2")
-    save_image(T1c, "name3")
-    save_image(T2, "name4")
-    save_image(OT, "name5")
+    #
+    # save_image(Flair, "name1")
+    # save_image(T1, "name2")
+    # save_image(T1c, "name3")
+    # save_image(T2, "name4")
+    # save_image(OT, "name5")
 
-
+    open_all_files("../data/med_images/input_image/BRATS2015/training/")
+    # open_all_files("D:/BRATS2015/training/")
